467717a05c81e52bd01bd6b3b7e8ae7eb974a9fd
KozichKonstantin <humanstepyt@gmail.com>
2023-09-10 23:13:07 +0300

    10.09.2023 V1.02
    aghhhh
    problems with selectors

--- ex2/testMasters/views/puttingHeroes.js
+++ ex2/testMasters/views/puttingHeroes.js
@@ -1,4 +1,5 @@
 // import {data} from './data.js';
+import{stats} from './data.js';
 // fetch('/putCard', function(response){
 //     method: "POST", 
 //     response.json().then(function(data){
@@ -21,49 +22,51 @@ async function postData(url = "", data = {}) {
         // for(let i=0; i < localStorage.getItem("number"); i++){
         // localStorage.setItem(`card${json.id}`, JSON.stringify(json))
         // }
-        function createAllCardElements(type, createdClass, place, text=""){
+        function createAllCardElements(type, createdClass, id, place, text=""){
             const createdType = document.createElement(type);
-            return createdClass && createdType.classList.add(...createdClass.split(" ")), text && (createdType.textContent = text), place.append(createdType)
+            return createdClass && createdType.classList.add(...createdClass.split(" ")), id && createdType.setAttribute(`id`,`${id}`), text && (createdType.textContent = text), place.append(createdType)
         }
         let savedList = document.querySelector(".savedList");
-        createAllCardElements('div', `savedCard ${json.id}`, savedList);
-        let savedCard = document.querySelectorAll(`.savedCard ${json.id}`);
-        createAllCardElements('div', `savedCardName ${json.id}`, savedCard);
-        let saveCardText =document.querySelectorAll(`.savedCardNme ${json.id}`);
-        createAllCardElements('h1', `savedCardNameText ${json.id}`, saveCardText);
-        createAllCardElements('div', `savedCardPicture ${json.id}`, savedCard);
-        saveCardText = document.querySelectorAll(`.savedCardPicture ${json.id}`);
-        createAllCardElements('img', `savedCardPictureImg ${json.id}`, saveCardText);
-        createAllCardElements('div', `savedCardSpecs ${json.id}`, savedCard);
-        saveCardText = document.querySelectorAll(`savedCardSpecs ${json.id}`);
+        createAllCardElements('div', `savedCard`, `${json.id}`, savedList);
+        let savedCard = document.querySelector(`${json.id}`,`.savedCard`);
+        console.log(savedCard, 'ada')
+        console.log(json.id)
+        createAllCardElements('div', `savedCardName`, `${json.id}`, savedCard);
+        let saveCardText =document.querySelector(`.savedCardName`, `#${json.id}`);
+        createAllCardElements('h1', `savedCardNameText`, `${json.id}`, saveCardText);
+        createAllCardElements('div', `savedCardPicture`, `${json.id}`, savedCard);
+        saveCardText = document.querySelector(`.savedCardPicture`, `#${json.id}`);
+        createAllCardElements('img', `savedCardPictureImg`, `${json.id}`, saveCardText);
+        createAllCardElements('div', `savedCardSpecs`, `${json.id}`, savedCard);
+        saveCardText = document.querySelector(`.savedCardSpecs`, `#${json.id}`);
         for(let i=0; i < 6; i++){
-            createAllCardElements('div', `specText ${sats[i].stat} ${json.id}`, saveCardText);
+            createAllCardElements('div', `specText ${stats[i].stat}`, `${json.id}`, saveCardText);
         }
-        saveCardText = document.querySelectorAll(`specText ${json.id}`)
-        createAllCardElements('h2', `specName ${json.id}`, saveCardText)
+        saveCardText = document.querySelector(`.specText`, `#${json.id}`)
+        createAllCardElements('h2', `specName`, `${json.id}`, saveCardText)
 
-        let cardsMassive = document.querySelectorAll(`.specText ${json.id}`)
-        let h2Fills = document.querySelectorAll(`specName ${json.id}`)
+        let cardsMassive = document.querySelector(`.specText`, `#${json.id}`)
+        let h2Fills = document.querySelector(`.specName`, `#${json.id}`)
         for( let i = 0; i < cardsMassive.length; i++){
             h2Fills.textContent = cardsMassive[i].classList.toString().substring(9);
         }
         for(let i =0; i < h2Fills.length; i++){
-            createAllCardElements('p', `specValue ${json.id}`, saveCardText)
+            createAllCardElements('p', `specValue`, `${json.id}`, saveCardText)
         }
-        createAllCardElements('div', `activityButtons_case ${json.id}`, savedCard)
-        let activityButtons_case = document.querySelectorAll(`.activityButtons_case`)
-        createAllCardElements('div', `activityButton reedFull ${json.id}`, activityButtons_case)
-        saveCardText = document.querySelectorAll(`.readFull ${json.id}`)
+        createAllCardElements('div', `activityButtons_case`, `${json.id}`, savedCard)
+        let activityButtons_case = document.querySelector(`.activityButtons_case`, `#${json.id}`)
+        createAllCardElements('div', `activityButton reedFull`, `${json.id}`, activityButtons_case)
+        saveCardText = document.querySelector(`.readFull`, `#${json.id}`)
         saveCardText.setAttribute('src', 'data/addit_images/free-file-icon-1453-thumb.png')
-        createAllCardElements('div', `activityButton printTo ${json.id}`, activityButtons_case)
-        saveCardText = document.querySelectorAll(`.printTo ${json.id}`)
-        createAllCardElements('img', `printToImg ${json.id}`, saveCardText);
-        saveCardText = document.querySelectorAll(`.printToImg ${json.id}`);
+        createAllCardElements('div', `activityButton printTo`, `${json.id}`, activityButtons_case)
+        saveCardText = document.querySelector(`.printTo`, `#${json.id}`)
+        createAllCardElements('img', `printToImg`, `${json.id}`, saveCardText);
+        saveCardText = document.querySelector(`.printToImg`, `#${json.id}`);
         saveCardText.setAttribute('src', 'data/addit_images/4305625.png');
-        createAllCardElements('div', `activityButton delete ${json.id}`, activityButtons_case )
-        saveCardText= document.querySelectorAll(`.delete ${json.id}`)
+        createAllCardElements('div', `activityButton delete`, `${json.id}`, activityButtons_case )
+        saveCardText= document.querySelector(`.delete`, `#${json.id}`)
         createAllCardElements('img', `deleteImg`, saveCardText);
-        saveCardText = document.querySelectorAll(`deleteImg`);
+        saveCardText = document.querySelector(`.deleteImg`, `#${json.id}`);
         createAllCardElements('src', 'data/addit_images/3481306.png')
 
 
@@ -78,18 +81,18 @@ for (let i=0; i<=(localStorage.getItem("number") - 1); i++){
     postData('/putCard', postForToServ);
 }    
     
-for (let i=0; i<=(localStorage.getItem("number") - 1); i++){
+// for (let i=0; i<=(localStorage.getItem("number") - 1); i++){
     // let postForToServ = {
     //     numb : i
     // }
     // postData('/putCard', postForToServ);
-    let savedBase = JSON.parse(localStorage.getItem(`card${i}`));
-    document.getElementsByClassName('savedCardNameText')[i].textContent = savedBase.class ;
-    document.getElementsByClassName('savedCardPictureImg')[i].src = savedBase.image+ ".jpg";
-    for(let j=3; j<9; j++){
-        document.getElementsByClassName('savedCardSpecs')[i].children[j].lastChild.textContent = savedBase[j];
-    }
-    }
+    // let savedBase = JSON.parse(localStorage.getItem(`card${i}`));
+    // document.getElementsByClassName('savedCardNameText')[i].textContent = savedBase.class ;
+    // document.getElementsByClassName('savedCardPictureImg')[i].src = savedBase.image+ ".jpg";
+    // for(let j=3; j<9; j++){
+    //     document.getElementsByClassName('savedCardSpecs')[i].children[j].lastChild.textContent = savedBase[j];
+    // }
+    // }
 
 
 
